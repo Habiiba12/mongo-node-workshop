d637b6831b5cd9f71ec9bedd5e53bf6aef583e03
habiiba12 <habiiba12@hotmail.com>
2017-08-26 16:25:01 +0100

    created dataBace

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,27 +1,26 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require("mongodb").ObjectID;
 
 router.get('/', function (req, res) {
-    const filePath = __dirname + '/../data/posts.json';
-    const callbackFunction = function(error, file) {
-        if(error) {
-            return next(error);
-        }
-        // we call .toString() to turn the file buffer to a String
-        const fileData = file.toString();
-        // we use JSON.parse to get an object out the String
-        const postsJson = JSON.parse(fileData);
-        // send the json to the Template to render
-        res.render('index', {
-          title: "Michael's profile",
-          subheading: "A modern Website built in Node with Handlebars",
-          posts: postsJson
-        });
-    };
-    fs.readFile(filePath, callbackFunction);
-});
+    // Write code to connect to database and return posts
+    const mongoConnection = 'mongodb://localhost:27017/profile';
 
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('posts').find({});
+        cursor.toArray((error, posts) => {
+            db.close();
+            // res.json(posts);
+            res.render('index', {
+                title: "Michael's profile",
+                subheading: "A modern Website built in Node with Handlebars",
+                posts: posts
+            });
+        });
+    });
+});
 
 router.get('/my-cv', function (req, res) {
     res.render('my-cv');
